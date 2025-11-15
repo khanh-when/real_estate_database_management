1115cc6a7d52c47c734314f5da4bede2548e72a1
khanh-when <tony.tnn.nguyen@gmail.com>
2025-11-15 14:12:30 -0600

    Relational Schema Created
    Continue implementation of Schema

--- data_parsing.py
+++ data_parsing.py
@@ -1,7 +1,18 @@
-data = {}
+
+ticker_data = {}
 
 with open('archive/symbols_valid_meta.csv') as f:
 
     line = f.readline().split(',')
 
-    print(line)
+    ticker_data[line[1]] = [] # securities
+    ticker_data[line[2]] = [] # s_name
+    ticker_data[line[3]] = [] # Listing
+    ticker_data[line[4]] = [] # Market Category
+    ticker_data[line[5]] = [] # ETF
+
+    print('\n',line)
+
+    print('\n',f.readline())
+
+    print('\n', ticker_data)
