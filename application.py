d810f90f916729cf746fa26cf3942db52cf5b9f5
khanh-when <tony.tnn.nguyen@gmail.com>
2025-11-21 19:57:31 -0600

    Data Pipeline Established
    Implement a method to insert all data into the database
    
    Stocks: 8049 entries
    Share Prices: 22,296,494 entries
    ETF Prices: 3,931,531 entries

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from data_parsers.stock_data_parser import stock_data
 from data_parsers.price_data_parser import price_data
 from data_parsers.reformat_data import reformat
 
+
 def connection(db_name):
     '''Establish Database Connection to MariaDB'''
 
@@ -29,7 +30,7 @@ def insert_stocks(conn, data, type=1):
 
         try:
             with conn.cursor() as cursor:
-                cursor.execute(query, (data[0], data[1], data[2], data[3], data[4]))
+                cursor.execute(query, data)
             print(f"Stock: {data[0]} is successfully added to the Stock Table.")
 
         except mariadb.Error as e:
@@ -55,35 +56,41 @@ def insert_prices(conn, data, type=1):
 
         try:
             with conn.cursor() as cursor:
-                cursor.execute(query, ('A', datetime.date(1999, 11, 18), 32.55, 35.77, 28.61, 27.07, 62546300))
-            print(f'Stock: {data[0]} | Date: {data[0]} | Successfully added to the PriceData Table.')
+                cursor.execute(query, data)
+            print(f'Stock: {data[0]} | Date: {data[1]} | Successfully added to the PriceData Table.')
         
         except mariadb.Error as e:
             print(f"Error: Insert - {e}")
             raise mariadb.Error
         
     if type == 2:
-        pass
+        
+        try:
+            with conn.cursor() as cursor:
+                cursor.executemany(query, data)
+            print(f"Successfully Inserted All Price Datas into the PriceData Table.")
+
+        except mariadb.Error as e:
+            print(f"Error: MariaDB several Insertions {e}")
+            raise mariadb.Error
 
 def main():
-    # share_data = reformat(stock_data(), type=1)
-    # for item in share_data:
-    #     print(item)
+    # all stock data in a list[tuple] data structure
+    share_data = reformat(stock_data(), type=1) 
+    print("All Stocks Datas:", len(share_data)) # 
 
-    share_prices = reformat(price_data('archive/stocks/*.csv'), type=2)
+    # all price data in a list[tuple] data structure
+    share_prices = reformat(price_data('archive/stocks/*.csv'), type=2) 
+    print("All Share Price Datas:", len(share_prices)) # 
 
-    for item in share_prices:
-        print(item)
-        break
-
-    # etf_prices = reformat(price_data('archive/etfs/*.csv'), type=2)
+    etf_prices = reformat(price_data('archive/etfs/*.csv'), type=2)
+    print("All ETF Price Datas:", len(etf_prices)) # 
 
     try:
 
         conn = connection('stock_market') # create cursor connection to stock_market db
-        # insert_stocks(conn, formatted, type=2) # insert all Stock/ETF .CSV metadata
-
-        insert_prices(conn, ['ok', 10], type=1)
+        # insert_stocks(conn, share_data, type=2) # insert all Stock/ETF .CSV metadata
+        # insert_prices(conn, share_prices, type=2)
 
     except Exception as e:
         print(f'Error: {e}')
